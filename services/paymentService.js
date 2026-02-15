88cc059548922d3af0a7ad310c41385db18c7cee
FaizanIrfan <muhammadfaizan0038@gmail.com>
2026-02-15 18:08:22 +0500

    currency changed to pkr

--- services/paymentService.js
+++ services/paymentService.js
@@ -3,8 +3,8 @@ const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
 exports.createPaymentIntent = async (amount) => {
   return await stripe.paymentIntents.create({
-    amount: amount * 100, // dollars to cents
-    currency: "usd",
+    amount: amount,
+    currency: "pkr",
     payment_method_types: ["card"],
   });
 };
