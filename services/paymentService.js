f7759a85bfbeaad19856678ebb9903965770bd19
FaizanIrfan <muhammadfaizan0038@gmail.com>
2026-02-16 13:11:50 +0500

    currency added

--- services/paymentService.js
+++ services/paymentService.js
@@ -4,6 +4,7 @@ const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 exports.createPaymentIntent = async (amount) => {
   return await stripe.paymentIntents.create({
     amount: amount * 100,
+    currency: "",
     payment_method_types: ["card"],
   });
 };
