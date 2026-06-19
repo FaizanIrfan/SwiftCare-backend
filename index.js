651e9318819b444ab588dca525f4a55cdc9a30f7
FaizanIrfan <muhammadfaizan0038@gmail.com>
2026-06-19 05:42:52 +0500

    Push

--- index.js
+++ index.js
@@ -148,7 +148,7 @@ initFirebaseAdmin();
    Server Start
 -------------------------------------------------- */
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7000;
 
 server.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
